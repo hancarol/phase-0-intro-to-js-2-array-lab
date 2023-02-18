feeba56f12376c1873d04d1c88f25716605c76fc
Caroline Ko <hancarol91@gmail.com>
2023-02-18 16:49:13 -0500

    commit tree 44393e20d878cf1bf042f7aa818479472e877eba

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+
+function appendCat(name) {
+  const newCats = [...cats, name];
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = [name, ...cats];
+  return newCats;
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
